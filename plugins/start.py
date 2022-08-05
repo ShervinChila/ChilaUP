725c3b939656d5e26b16244c525f0344e7b13d64
ShervinChila <108692453+ShervinChila@users.noreply.github.com>
2022-08-06 01:14:22 +0430

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -122,7 +122,7 @@ async def not_joined(client: Client, message: Message):
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text = 'Try Again',
+                    text = 'امتحان مجدد',
                     url = f"https://t.me/{client.username}?start={message.command[1]}"
                 )
             ]
