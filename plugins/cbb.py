5387724eed38a1612921f86420373b30dcdec991
ShervinChila <108692453+ShervinChila@users.noreply.github.com>
2022-11-05 14:07:19 -0500

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>🧛 سازنده : شـروین\n🏠 خونه زندگیم : <a href='https://t.me/indiancnema/'>چنل سینمای هند </a>\n👧🏻 حرف و حدیثی با ما : <a href='https://t.me/kobiii1997/'>ادمین </a>\n🌏 وبسایت ما : در حال ساخت </b>",
+            text = f"<b>🧛 سازنده : شـروین\n🏠 خونه زندگیم : <a href='https://t.me/irbollywoodorg1/'>چنل ایران بالیوود </a>\n👧🏻 حرف و حدیثی با ما : رئیس کوچولو فعلا آیدی نداره بذاریم </a>\n🌏 وبسایت ما : در حال ساخت </b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
